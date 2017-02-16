544d34eebdb625d7b2192fe559dabc32d82f9b76
Vitalik <vitalikfmne@mail.ru>
2017-02-16 22:31:41 +0200

    Lesson_12

--- C++VS/Exersice/Lessons12/function_arguments/concatenation/Source.cpp
+++ C++VS/Exersice/Lessons12/function_arguments/concatenation/Source.cpp
@@ -6,16 +6,24 @@ using namespace std;
 
 char* cat(const char str1[], const char str2[])
 {
-	int length;
-	length = strlen(str1);
-	length += strlen(str2);
-	char arr[length + 1];
-	for(int i(0); i < length )
+	int length1 = strlen(str1);
+	int length2 = strlen(str2);	
+	char* arr = new char[length1 + length2 + 1];
+	for (int i(0); i < length1; i++)
+		arr[i] = str1[i];
+	for (int i(0); i < length2; i++)
+		arr[i + length1] = str2[i];
+	arr[length1 + length1] = '\0';
+	return arr;
 }
 
 int main()
 {
-
+	char str1[] = "Hello";
+	char str2[] = " World\n";
+	char* concatination = cat(str1, str2);
+	cout << concatination;
+	delete[] concatination;
 	_getch();
 	return 0;
 }
